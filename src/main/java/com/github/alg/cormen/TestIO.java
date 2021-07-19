20b8c48e4b3dfbc17403d05e3ccd6e9467d97262
Abhishek Verma <59361086+abhishek-verma-github@users.noreply.github.com>
2021-07-19 20:32:10 +0530

    exception handled.

--- src/main/java/com/github/alg/cormen/TestIO.java
+++ src/main/java/com/github/alg/cormen/TestIO.java
@@ -7,30 +7,33 @@ import java.util.Iterator;
 
 public class TestIO {
     public static void main(String[] args) {
+        ArrayList<String> str_in = new ArrayList<>();
+        BufferedReader br = null;
+        FileOutputStream fout = null;
+        String input;
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            ArrayList<String> str_in = new ArrayList<>();
-
-            String input;
+            br = new BufferedReader(new InputStreamReader(System.in));
+            fout = new FileOutputStream("test.txt");
             do{
                 input = br.readLine();
                 if(input.equals("!q")) break;
                 str_in.add(input);
             }while(!input.equals("!q"));
 
-            FileOutputStream fout = new FileOutputStream("test.txt");
-
             Iterator<String> itr = str_in.iterator();
-
 
             while(itr.hasNext()){
                 fout.write(itr.next().toString().getBytes(StandardCharsets.UTF_8));
                 fout.write("\n".getBytes(StandardCharsets.UTF_8));
             }
         }
-        catch(IOException e){
-            e.printStackTrace();
+        catch (IOException e){e.printStackTrace();}
+        finally {
+            try {
+                if(fout != null) fout.close();
+            }catch(IOException e){e.printStackTrace();}
         }
+
 
     }
 }
